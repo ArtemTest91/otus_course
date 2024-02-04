dd7dd4ae82e4127b950e7db9d811d4f3b18bd24d
ArtemTest91 <artemairzgt@gmail.com>
2024-02-04 22:07:32 +0400

    .

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,33 +1,36 @@
 import csv
 import json
+from otus_course.hw4 import CSV_FILE, JSON_FILE
 
-from otus_course.hw4 import CSV_FILE
-from otus_course.hw4 import JSON_FILE
 
 def read_books_from_csv():
-    with open(CSV_FILE, newline='') as books:
-        books_read = csv.DictReader(books)
-        books_data = list(books_read)
-
-    return books_data
+    try:
+        with open(CSV_FILE, newline='') as books_file:
+            books_reader = csv.DictReader(books_file)
+            return list(books_reader)
+    except FileNotFoundError:
+        print(f"Ошибка: Файл {CSV_FILE} не найден.")
+        return []
 
 
 def read_users_from_json():
-    with open(JSON_FILE) as users:
-        users_data = json.load(users)
-
-    return users_data
+    try:
+        with open(JSON_FILE) as users_file:
+            return json.load(users_file)
+    except FileNotFoundError:
+        print(f"Ошибка: Файл {JSON_FILE} не найден.")
+        return []
 
 
 def load_data():
-    books_data = read_books_from_csv()
-    users_data = read_users_from_json()
+    books = read_books_from_csv()
+    users = read_users_from_json()
 
-    for book in books_data:
-        book['pages'] = int(book['Pages'])
-        del book['Pages']
+    for book in books:
+        if 'Pages' in book:
+            book['pages'] = int(book.pop('Pages'))
 
-    return books_data, users_data
+    return books, users
 
 
 def distribute_books(books, users):
@@ -46,28 +49,30 @@ def distribute_books(books, users):
 
         for _ in range(books_per_user):
             user_books.append({
-                "title": books[book_index]["Title"],
-                "author": books[book_index]["Author"],
-                "pages": books[book_index]["pages"],
-                "genre": books[book_index]["Genre"]
+                "title": books[book_index].get("Title", ""),
+                "author": books[book_index].get("Author", ""),
+                "pages": books[book_index].get("pages", 0),
+                "genre": books[book_index].get("Genre", "")
             })
-            book_index = (book_index + 1) % total_books
+
+            book_index += 1
 
         if remaining_books > 0:
             user_books.append({
-                "title": books[book_index]["Title"],
-                "author": books[book_index]["Author"],
-                "pages": books[book_index]["pages"],
-                "genre": books[book_index]["Genre"]
+                "title": books[book_index].get("Title", ""),
+                "author": books[book_index].get("Author", ""),
+                "pages": books[book_index].get("pages", 0),
+                "genre": books[book_index].get("Genre", "")
             })
-            book_index = (book_index + 1) % total_books
+
+            book_index += 1
             remaining_books -= 1
 
         result.append({
-            "name": user["name"],
-            "gender": user["gender"],
-            "address": user["address"],
-            "age": user["age"],
+            "name": user.get("name", ""),
+            "gender": user.get("gender", ""),
+            "address": user.get("address", ""),
+            "age": user.get("age", 0),
             "books": user_books
         })
 
@@ -80,6 +85,9 @@ def save_result(result):
 
 
 if __name__ == "__main__":
-    books_data, users_data = load_data()
-    result_data = distribute_books(books_data, users_data)
-    save_result(result_data)
+    books, users = load_data()
+    if not books or not users:
+        print("Ошибка при загрузке данных. Пожалуйста, проверьте наличие файлов.")
+    else:
+        result_data = distribute_books(books, users)
+        save_result(result_data)
