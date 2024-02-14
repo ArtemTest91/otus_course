0fdec066414be48e56c6583f18587203f75c3003
ArtemTest91 <artemairzgt@gmail.com>
2024-02-14 22:18:35 +0400

    /

--- hw5/test_dog_api.py
+++ hw5/test_dog_api.py
@@ -4,20 +4,6 @@ import pytest
 BASE_URL_DOG = 'https://dog.ceo/api'
 breeds_to_test = ["chihuahua", "poodle", "beagle", "husky", "maltese"]
 
-def test_random_dog():
-    response = requests.get(f"{BASE_URL_DOG}/breeds/image/random")
-    assert response.status_code == 200
-    assert "message" in response.json()
-    assert response.json()["status"] == "success"
-
-
-def test_list_all_breeds():
-    response = requests.get(f"{BASE_URL_DOG}/breeds/list/all")
-    assert response.status_code == 200
-    assert "message" in response.json()
-    assert isinstance(response.json()["message"], dict)
-    assert "bulldog", "bullterrier" in response.json()["message"]
-
 
 @pytest.mark.parametrize("breed", breeds_to_test)
 def test_specific_breed_images(breed):
@@ -45,3 +31,18 @@ def test_random_image_for_breed(breed):
     assert "message" in response.json()
     assert isinstance(response.json()["message"], str)
     assert response.json()["message"].endswith(".jpg")
+
+
+def test_random_dog():
+    response = requests.get(f"{BASE_URL_DOG}/breeds/image/random")
+    assert response.status_code == 200
+    assert "message" in response.json()
+    assert response.json()["status"] == "success"
+
+
+def test_list_all_breeds():
+    response = requests.get(f"{BASE_URL_DOG}/breeds/list/all")
+    assert response.status_code == 200
+    assert "message" in response.json()
+    assert isinstance(response.json()["message"], dict)
+    assert "bulldog", "bullterrier" in response.json()["message"]
