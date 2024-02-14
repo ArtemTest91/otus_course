705d376456e189bb35a4b173c2b6fd35490b32e8
ArtemTest91 <artemairzgt@gmail.com>
2024-02-15 00:41:59 +0400

    /

--- hw5/conftest.py
+++ hw5/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="https://ya.ru", help="URL для теста")
     parser.addoption("--status_code", action="store", default=200, type=int, help="Ожидаемый код состояния")
